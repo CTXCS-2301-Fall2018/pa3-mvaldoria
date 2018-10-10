6054340b1e7f444e6ccb199b885d523140bf3941
Marc Valdoria <marc.valdoria@ctx.edu>
2018-10-10 15:41:06 -0500

    PA3-2 - attempt 1

--- PA3-2.s
+++ PA3-2.s
@@ -33,27 +33,9 @@ main:
 
     @Your modifications will begin at this point
 
-    CMP R1, #1       @Check for "peanuts"
-    BEQ _peanut      @If user entered 1 goto _peanuts
-    CMP R1, #2       @Check for "chocolate"
-    BEQ _choc        @If user entered 2 goto _choc
-    CMP R1, #3       @Check for "pretzels"
-    BEQ _pretzel     @If user entered 3 goto _pretzel
-    LDR R0, =msg7    @If we get here user entered
-                     @an illegal selection so print
-                     @error message and terminate
-    BL printf
-    MOV R7, #1
-    SWI #0           @Terminate, error condition
-_peanut:
-    MOV R3, #75      @Move 75 cents into R3
-    BAL _compute
-_choc:
-    MOV R3, #125     @Move 125 cents into R3
-    BAL _compute
-_pretzel:
-    MOV R3, #90      @Move 90 cents into R3
-_compute:
+@PEANUTS
+    CMP R1, #1        @Check for "peanuts"
+    MOVEQ R3, #75     @Move 75 cents into R3 if R1 is equal to #1
     LDR R4, =quantity @Get address of var quantity
     LDR R4, [R4]      @Value of quantity now in R4
     MUL R1, R3, R4    @Multiply number of cents times quantity
@@ -62,6 +44,38 @@ _compute:
     BL printf
     MOV R7, #1        @Normal exit
     SWI #0
+
+@CHOCOLATE
+    CMP R1, #2        @Check for "chocolate"
+    MOVEQ R3, #125    @Move 125 cents into R3
+    LDR R4, =quantity @Get address of var quantity
+    LDR R4, [R4]      @Value of quantity now in R4
+    MUL R1, R3, R4    @Multiply number of cents times quantity
+                      @and put result in R1
+    LDR R0, =msg6     @Final message
+    BL printf
+    MOV R7, #1        @Normal exit
+    SWI #0
+
+@PRETZELS
+    CMP R1, #3        @Check for "pretzels"
+    MOVEQ R3, #90     @Move 90 cents into R3
+    LDR R4, =quantity @Get address of var quantity
+    LDR R4, [R4]      @Value of quantity now in R4
+    MUL R1, R3, R4    @Multiply number of cents times quantity
+                      @and put result in R1
+    LDR R0, =msg6     @Final message
+    BL printf
+    MOV R7, #1        @Normal exit
+    SWI #0
+
+
+    LDR R0, =msg7    @If we get here user entered
+                     @an illegal selection so print
+                     @error message and terminate
+    BL printf
+    MOV R7, #1
+    SWI #0           @Terminate, error condition
 
 .data
 select: .word 0
